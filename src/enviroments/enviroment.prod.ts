264deb6d5c16269ace4454936272247da2ff60e2
Carlos Hincapie <caehinca@bancolombia.com.co>
2026-07-16 08:52:19 -0500

    fix: update API URL in production environment

--- src/enviroments/enviroment.prod.ts
+++ src/enviroments/enviroment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://obav35v2n2.execute-api.us-east-1.amazonaws.com/prod',
+  apiUrl: 'https://5bi71g09s2.execute-api.us-east-1.amazonaws.com/prod ',
 };
