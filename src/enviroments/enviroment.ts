53c8ef4aa508d255cc8f0b87ce92ad2fecfa244c
Carlos Hincapie <caehinca@bancolombia.com.co>
2026-07-21 00:47:39 -0500

    fix: remove commented-out API URLs from environment configuration

--- src/enviroments/enviroment.ts
+++ src/enviroments/enviroment.ts
@@ -1,5 +1,3 @@
 export const environment = {
-  //apiUrlPdn: 'https://87nvsjhj8e.execute-api.us-east-1.amazonaws.com/prod',
-  //apiUrlStaging: 'https://rv5lapopud.execute-api.us-east-1.amazonaws.com/staging';
   apiUrl: 'http://127.0.0.1:3000',
 };
